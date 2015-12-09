3d9e7e70653321babb1b17cc68716915d9186111
Daniel Viorreta <daniel.viorreta@infojobs.net>
2015-12-09 17:08:05 +0100

    Translation of about and help pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,22 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", I18n.t('static_pages.home.title')
+    assert_select "title", I18n.t('base_title')
+    assert_select "h1", I18n.t('static_pages.home.header')
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", I18n.t('static_pages.help.title')
+    assert_select "title", I18n.t('static_pages.help.title') + ' | ' + I18n.t('base_title')
+    assert_select "h1", I18n.t('static_pages.help.header')
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", I18n.t('static_pages.about.title')
+    assert_select "title", I18n.t('static_pages.about.title') + ' | ' + I18n.t('base_title')
+    assert_select "h1", I18n.t('static_pages.about.header')
   end
 
 end
