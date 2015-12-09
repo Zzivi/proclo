be7c228436212bce51fb2f90c7758976663e172f
Daniel Viorreta <daniel.viorreta@infojobs.net>
2015-12-09 16:41:14 +0100

    Transaltion for titles of static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,19 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Proclo App"
+    assert_select "title", I18n.t('static_pages.home.title')
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Proclo App"
+    assert_select "title", I18n.t('static_pages.help.title')
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Proclo App"
+    assert_select "title", I18n.t('static_pages.about.title')
   end
 
 end
