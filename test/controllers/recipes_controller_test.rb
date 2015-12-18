7824577aa0df41fae4145a7699a85eac13704085
Daniel Viorreta <daniel.viorreta@infojobs.net>
2015-12-18 12:15:17 +0100

    Add test for recipes index page

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -14,4 +14,13 @@ class RecipesControllerTest < ActionController::TestCase
     assert_select 'title', "#{I18n.t('recipes.index.title')} | #{@base_title}"
     assert_select 'h1', I18n.t('recipes.index.title')
   end
+
+  test "index" do
+    get :index
+    assert_select 'div.pagination'
+    Recipe.paginate(page: 1).each do |recipe|
+      assert_select 'a[href=?]', recipe_path(recipe), text: recipe.name
+    end
+    assert_response :success
+  end
 end
