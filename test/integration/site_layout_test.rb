d0f23849c3b6b1759fb9d2c68f85e05480c284e9
Daniel Viorreta <daniel.viorreta@infojobs.net>
2015-12-18 13:16:19 +0100

    Add links to list and create recipes in the home page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -8,7 +8,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
+  end
+
+  test "ingredients links" do
     assert_select "a[href=?]", ingredients_path
     assert_select "a[href=?]", new_ingredient_path
+  end
+
+  test "recipes links" do
+    assert_select "a[href=?]", recipes_path
+    assert_select "a[href=?]", new_recipe_path
   end
 end
