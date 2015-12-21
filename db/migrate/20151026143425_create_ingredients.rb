53f2ac6ae18299f662d240c98dc6e50bd09f530e
Daniel Viorreta <daniel.viorreta@infojobs.net>
2015-12-21 12:17:18 +0100

    Change ingredient description field to text

--- db/migrate/20151026143425_create_ingredients.rb
+++ db/migrate/20151026143425_create_ingredients.rb
@@ -2,7 +2,7 @@ class CreateIngredients < ActiveRecord::Migration
   def change
     create_table :ingredients do |t|
       t.string :name
-      t.string :description
+      t.text :description
       t.integer :measurement_type, default: 0, null: false
       t.timestamps null: false
     end
